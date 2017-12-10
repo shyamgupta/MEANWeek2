f2ee3b4b29e24526e1c6e8235e8bd2c6604a1e90
Shyam Gupta <postboxshyam@gmail.com>
2017-12-10 00:42:28 -0500

    Hunt GitHub using Promises

---
+++ regform/src/app/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        public firstName: string = "",
+        public lastName:string = "",
+        public email: string = "",
+        public password: string = "",
+        public street: string = "",
+        public unit: string = "",
+        public city: string = "",
+        public state: string = "",
+        public feel: string = ""
+    ){}
+}
